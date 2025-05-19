9fb739bfd47976939c681078df637eb86b1b43d8
Aftopelot <156015708+Aftopelot@users.noreply.github.com>
2025-05-19 15:16:37 +0300

    Update kerber.js

--- kerber.js
+++ kerber.js
@@ -30,7 +30,7 @@ new RGBELoader()
   .load('assets/neutral.hdr', (texture) => {
     texture.mapping = THREE.EquirectangularReflectionMapping;
     scene.environment = texture;
-    scene.background = new THREE.Color(0x222222);
+    scene.background = new THREE.Color(0E403F);
     loadModel();
   });
 
